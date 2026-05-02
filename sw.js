e02a5b451d1a3b531a6b6c1a1bbf6b2f705a3ee2
OYKLd <lydieouattara828@gmail.com>
2026-05-02 22:14:16 +0000

    j

--- sw.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener("fetch", (event) => {
-  // Pour l'instant, on laisse passer toutes les requêtes
-  // Vous pouvez ajouter ici une logique de cache plus tard
-  event.respondWith(fetch(event.request));
-});
